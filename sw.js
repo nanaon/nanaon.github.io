2d88b818e6b90f2590e7abd70a9ca5a5fbe7fd0e
nanaon <naon.jeong@gmail.com>
2021-07-13 23:53:05 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c9e4fbf893a44e9cf798.js"
+    "url": "webpack-runtime-2f23a90d4f4c291fe2e4.js"
   },
   {
     "url": "styles.bf76a96084a01091552c.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1a9233ee89d76268416942d7e7ea9026"
+    "revision": "02554504bef7ff36cbfbc5a2f89adf77"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e8e7fbb006d57d32ed40.js"
